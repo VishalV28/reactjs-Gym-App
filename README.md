401d081ef08a950557be5f3e8472bd11fe8a8d92
VishalV28 <vishalvich@gmail.com>
2024-05-08 19:23:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# reactjs-Gym-App
+ Fitness Application using react.js and Tailwind CSS
---
+++ reactjs-fitness-app/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
+        <p>TIME TO GET</p>
+        <h1 className='uppercase '>MASSIVE</h1>
+        <p>djahfhuiohosslnd<span>abiiuwhh</span> dklsskksa<span>sjdkjsijj</span> cslnnnslnlnlns</p>
+    </div>
+  )
+}
