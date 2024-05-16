60c2897974ce26d53217f49edb66da0856adc408
VishalV28 <vishalvich@gmail.com>
2024-05-15 21:02:42 -0700

    Workout generator functionality

--- reactjs-fitness-app/src/components/Hero.jsx
+++ reactjs-fitness-app/src/components/Hero.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
+      <div className='flex flex-col gap-4'>
         <p>TIME TO GET</p>
-        <h1 className='uppercase '>MASSIVE</h1>
-        <p>djahfhuiohosslnd<span>abiiuwhh</span> dklsskksa<span>sjdkjsijj</span> cslnnnslnlnlns</p>
+        <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>MASSIVE</h1>
+      </div>
+      <p className='text-sm md:text-base font-light'> djahfhuiohosslnd <span className='text-blue-400 font-medium'>abiiuwhh</span> dklsskksa 
+      <span className='text-blue-400 font-medium'>sjdkjsijj</span> cslnnnslnlnlns</p>
     </div>
   )
 } //c
